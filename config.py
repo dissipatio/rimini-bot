9cb410a83ec3909fd79c8dfd7816b378b8850d28
dissipatio <65160119+dissipatio@users.noreply.github.com>
2026-05-16 18:14:10 +0200

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ FIELD_CHAT_ID = os.environ.get("FIELD_CHAT_ID", "chat_id")
 FIELD_CURRENT_STEP = os.environ.get("FIELD_CURRENT_STEP", "current_step")
 FIELD_LANGUAGE = os.environ.get("FIELD_LANGUAGE", "language")
 
-FFIRST_STEP = {
+FIRST_STEP = {
     "rus": "1_1_r",
     "eng": "1_1_r",
     "ita": "1_1_r",
