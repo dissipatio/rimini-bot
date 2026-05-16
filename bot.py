dd9edbc7d8bd67f36a2dd4ed05dc8214b9ea3f2c
dissipatio <dissipatio.hg@gmail.com>
2026-05-16 10:18:47 +0200

    fix url_path webhook

--- bot.py
+++ bot.py
@@ -144,6 +144,7 @@ if WEBHOOK_URL:
     app.run_webhook(
         listen="0.0.0.0",
         port=PORT,
+        url_path="webhook",
         webhook_url=WEBHOOK_URL,
         drop_pending_updates=True,
     )
